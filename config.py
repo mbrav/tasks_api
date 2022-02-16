b567444cb74dbff0bbe5ab45507f0e694f707484
mbrav <mbrav@protonmail.com>
2022-02-16 15:19:49 +0300

    use sqlite for debug = True

--- config.py
+++ config.py
@@ -10,18 +10,20 @@ env_path = Path('.env')
 load_dotenv(dotenv_path=env_path)
 
 
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = os.getenv('DEBUG', True)
 
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN', None)
 
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER', None)
+DB_PASSWORD = os.getenv('DB_PASSWORD', None)
+DB_HOST = os.getenv('DB_HOST', None)
+DB_PORT = os.getenv('DB_PORT', None)
+DB_NAME = os.getenv('DB_NAME', None)
 
-DATABASE_URL = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}?charset=utf8mb4'
-# DATABASE_URL = 'sqlite:///./api.db'
+if DEBUG:
+    DATABASE_URL = 'sqlite:///./api.db'
+else:
+    DATABASE_URL = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}?charset=utf8mb4'
 
 logging.basicConfig(
     filename='app.log',
